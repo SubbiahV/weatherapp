55fc39e49ff8711b967eec84f47502d17018d155
SubbiahV <20tuee130@skct.edu.in>
2024-05-19 12:49:58 +0530

    change in app

--- src/App.js
+++ src/App.js
@@ -8,18 +8,25 @@ const api = {
   key: '2c3297c3c1c249a4f3ad887a7c8888f0',
   base: 'https://api.openweathermap.org/data/2.5/'
 }
+// ... (import statements and other code above)
 
 function App() {
-
-  const [value,setvalue]=useState('');
+  const [value, setValue] = useState('');
   const [weather, setWeather] = useState({});
+  const [searched, setSearched] = useState(false);
 
   const Press = () => {
-     fetch(`${api.base}weather?q=${value}&units=metric&APPID=${api.key}`)
-     .then((res) => res.json())
-     .then((result) => {
-       setWeather(result);
-     });
+    if (value.trim() !== '') {
+      fetch(`${api.base}weather?q=${value}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+        })
+        .catch(() => {
+          setWeather({});
+        });
+      setSearched(true);
+    }
   };
 
   return (
@@ -27,24 +34,31 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <div>
-        <input className='inp' type='text' placeholder='Enter city' onChange={(e) => setvalue(e.target.value)} ></input>
-        <button className='buton' onClick={Press}><b>Search</b></button>
+          <input
+            className='inp'
+            type='text'
+            placeholder='Enter city..'
+            onChange={(e) => setValue(e.target.value)}
+          ></input>
+          <button className='buton' onClick={Press}><b>Search</b></button>
         </div>
-        {typeof weather.main !== "undefined" ? (
-          <div>
-            {/* Location  */}
-            <p>{weather.name}</p>
+        {searched ? (
+          typeof weather.main !== "undefined" ? (
+            <div>
+              {/* Location  */}
+              <p>{weather.name}</p>
 
-            {/* Temperature Celsius  */}
-            <p>{weather.main.temp}°C</p>
+              {/* Temperature Celsius  */}
+              <p>{weather.main.temp}°C</p>
 
-            {/* Condition (Sunny ) */}
-            <p>{weather.weather[0].main}</p>
-            <p>({weather.weather[0].description})</p>
-          </div>
-        ) : (
-          <p>Check spelling</p>
-        )}
+              {/* Condition (Sunny ) */}
+              <p>{weather.weather[0].main}</p>
+              <p>({weather.weather[0].description})</p>
+            </div>
+          ) : (
+            <div><p>Invalid city or network error</p></div>
+          )
+        ) : null}
       </header>
     </div>
   );
